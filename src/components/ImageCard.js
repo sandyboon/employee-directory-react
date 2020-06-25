bb6aa5c289e0ec443ac7dac4a2009251f076cfdc
ssingh <sandy.boon@gmail.com>
2020-06-25 13:11:23 -0400

    Added new data and improved the employee card a bit

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,15 +4,27 @@ function ImageCard(props) {
   // id={props.emp.id}
   return (
     <div className="card" style={{ width: '18rem' }}>
-      <img className="card-img-top" src="..." alt="Card  cap"></img>
+      <img
+        className="card-img-top"
+        src={props.emp.image}
+        alt={props.emp.name + '_avatar'}
+      ></img>
       <div className="card-body">
         <h5 className="card-title">{props.emp.name}</h5>
         <h6 className="card-subtitle mb-2 text-muted">{props.emp.jobTitle}</h6>
         <div>
-          <p className="card-text">Department: {props.emp.department}</p>
-        </div>
-        <div>
-          <p className="card-text">Office: {props.emp.officeLocation}</p>
+          <span style={{ display: 'inline-block' }} className="card-text">
+            Department: {props.emp.department}
+          </span>
+          <span style={{ display: 'inline-block' }} className="card-text">
+            Office: {props.emp.officeLocation}
+          </span>
+          <span style={{ display: 'inline-block' }} className="card-text">
+            Phone: {props.emp.workPhone}
+          </span>
+          <span style={{ display: 'inline-block' }} className="card-text">
+            {props.emp.email}
+          </span>
         </div>
       </div>
     </div>
